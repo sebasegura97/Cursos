ff365e31cee4ad2c296cafd653fe1750276630e8
Sebastian Segura <sebasegura97@gmail.com>
2019-02-02 15:57:26 -0300

    Angular basics

---
+++ angular/platzinger/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MenuComponent } from './menu/menu.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
